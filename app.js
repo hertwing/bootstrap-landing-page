493337bd701c192c0b55349e77c26102a2d4b99c
Piotr Henczel <henczel.piotr@gmail.com>
2018-10-01 21:29:27 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,32 @@
+var main = function(){
+	//scrollspy animation
+	$('body').scrollspy({ target: ".scrollBtn", offset: 0 });
+
+    $("#learnMore").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function() {
+                window.location.hash = hash;
+            });
+        }
+    });
+	//prevent default submit
+    $('#formSubmit').click(function(e) {
+        e.preventDefault();
+    });
+    //prevent default submit end
+
+    //main view animations
+    $('#companyDescription').removeClass("hidden").hide().show("drop", { direction: "left" }, 750);
+
+    $('#formWrapper').removeClass("hidden").hide().show("drop", { direction: "right" }, 750);
+    //main view animations end
+}	
+
+$(document).ready(main);
+	
